345befad9d559b33acc0c56188427b8f2fbdc99b
Justin Litchfield <litch@me.com>
2013-02-05 12:29:57 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LsrPortal::Application.config.secret_token = 'af4a041ab570f5b6231256503bfa2d4de14f7af6f7268587781cf04137d200ad3973255f2274ba6eb26c76532952cdf7dbe1f689b720e8c1326bf770957ce41f'
